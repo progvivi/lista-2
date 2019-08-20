59a41b251e27958b5477a72f99015b4617b96f82
progvivi <49981674+progvivi@users.noreply.github.com>
2019-08-20 09:39:06 -0300

    Update prog001.c

--- prog001.c
+++ prog001.c
@@ -1,22 +1,60 @@
 #include <stdio.h>
-#include <math.h>
-#include <string.h>
+#include <time.h>
 #include <stdlib.h>
+#define TAMANHO 4
 
-void main(){
-EXERCICIO 1
-int numero;
-printf("digite um numero: \n");
-scanf("%d", &numero);
+void main() {
+    int  vetor[TAMANHO]; 
+    int aux = 0; 
+    int long cont = 0;
+    int long troca = 0;
+    clock_t tempoInicial, tempoFinal; 
+    srand(time(NULL)); 
+    tempoInicial = clock(); 
+    for (int i = 0; i < TAMANHO; i++) {
+        vetor[i] = rand() % 10; 
+    }
+    
+    for (int i = 0; i < TAMANHO; i++) {
+        printf("%d\t", vetor[i]);
+    }
+    printf("\n");
+    
+    for (int i = 1; i < TAMANHO; i++) {
+        for (int j = 0; j < TAMANHO - 1; j++) {
+            if (vetor[j] > vetor[j + 1]) {
+                aux = vetor[j];
+                vetor[j] = vetor[j + 1];
+                vetor[j + 1] = aux;
+                troca = troca + 1;
+            }
+            cont = cont + 1;
+        }
+        printf("\n");
+    }
+     for (int i = 0; i < TAMANHO; i++) {
+        printf("%d\t", vetor[i]);
+     }
+    for (int i = 1; i < TAMANHO; i++) {
+        for (int j = 0; j < TAMANHO - 1; j++) {
+            if (vetor[j] < vetor[j + 1]) {
+                aux = vetor[j];
+                vetor[j] = vetor[j + 1];
+                vetor[j + 1] = aux;
+                troca = troca + 1;
+            }
+            cont = cont + 1;
+        }
+        printf("\n");
+    }
+    
+    for (int i = 0; i < TAMANHO; i++) {
+        printf("%d\t", vetor[i]);
+    }
+    printf("\n");
+    printf("qtd de comparaçao:   %ld\n", cont);
+    printf("qtd de troca:   %ld \n", troca);
+    tempoFinal = clock(); 
+    printf("Tempo: %f s\n", (double) (tempoFinal - tempoInicial) / CLOCKS_PER_SEC);
 
-if(numero %2 == 0){
-	printf("se o numero for par: %d\n", numero);
-	
-}
-
-	else{
-
-	printf("se o numero dor impar: %d\n", numero);
-
-	}
-}
+}                    
