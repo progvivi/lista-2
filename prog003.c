32e7e4a0b6d230f7df4a88ffe45b4289c24407eb
progvivi <49981674+progvivi@users.noreply.github.com>
2019-08-21 08:17:17 -0300

    Update prog003.c

--- prog003.c
+++ prog003.c
@@ -1,26 +1,67 @@
 #include <stdio.h>
-#include <math.h>
-#include <string.h>
+#include <time.h>
 #include <stdlib.h>
+#define TAMANHO 4
 
-void main(){
-int positivo = 1;
-int negativo = 0;
-int x,y;
-printf("digie o valor de x:\n");
-scanf("%d",&x);
-printf("digie o valor de y:\n");
-scanf("%d",&y);
-	if(x >=1 && y>=1 ){
-printf("primeiro quadrante.\n");
-	}
-	else if(x >= 1 && y < 0, x < 0 && y>=1){
-printf("segundo quadrante.\n");
-	}
-	else if(x < 0 && y < 0){
-printf("terceiro quadrante.\n");
-	}	
-	else{
-printf("quarto quadrante.\n");
-	}
+void main() {
+    int vetor[TAMANHO];
+    int eleito = 0;
+    int j = 0;
+    int long cont = 0;
+    int long troca = 0;
+    clock_t tempoInicial, tempoFinal;
+    srand(time(NULL));
+    tempoInicial = clock();
+    for (int i = 0; i < TAMANHO; i++) {
+        vetor[i] = rand() % 10;
+        
+    }
+    printf("\n");
+    for (int i = 0; i < TAMANHO; i++) {
+        printf("%d\t", vetor[i]);
+    }
+    printf("\n");
+ 
+    for (int i = 1; i < TAMANHO; i++) {
+        eleito = vetor[i];
+        j = i - 1;
+        while (j >= 0 && vetor[j] > eleito) {
+            vetor[j + 1] = vetor[j];
+            j--;
+            troca = troca+1;
+        }
+        cont = cont+1;
+        vetor[j + 1] = eleito;
+    }
+   
+
+    for (int i = 0; i < TAMANHO; i++) {
+        printf("%d\t", vetor[i]);
+        
+    }
+    printf("\n");
+       printf("qtd de comparaçao:   %ld\n", cont);
+    printf("qtd de troca:   %ld \n", troca);
+    printf("\n");
+    for (int i = 1; i < TAMANHO; i++) {
+        eleito = vetor[i];
+        j = i - 1;
+        while (j >= 0 && vetor[j] < eleito) {
+            vetor[j + 1] = vetor[j];
+            j--;
+            troca = troca+1;
+        }
+        cont = cont+1;
+        vetor[j + 1] = eleito;
+    }
+    for (int i = 0; i < TAMANHO; i++) {
+        printf("%d\t", vetor[i]);
+    }
+        printf("\n");
+    printf("qtd de comparaçao:   %ld\n", cont);
+    printf("qtd de troca:   %ld \n", troca);
+    tempoFinal = clock();
+
+
+    printf("Tempo: %f s\n", (double) (tempoFinal - tempoInicial) / CLOCKS_PER_SEC);
 }
